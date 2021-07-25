591d47dac971f40581750716311a05a435ae1fbe
James Zhang <jameszhanga@gmail.com>
2021-07-25 17:04:03 -0500

    Able to select and change page color

---
+++ app.js
@@ -0,0 +1,29 @@
+const defaultBtn = document.querySelector('#default');
+const darkBtn = document.querySelector('#dark');
+const lightBtn = document.querySelector('#light');
+const body = document.body;
+
+const theme = localStorage.getItem('theme');
+body.classList.add('default');
+if (theme) {
+  body.classList.remove('default', 'dark', 'light');
+  body.classList.add(theme);
+}
+
+defaultBtn.addEventListener('click', () => {
+  body.classList.add('default');
+  body.classList.remove('dark', 'ligh');
+  localStorage.setItem('theme', 'default');
+});
+
+darkBtn.addEventListener('click', () => {
+  body.classList.add('dark');
+  body.classList.remove('default', 'light');
+  localStorage.setItem('theme', 'dark');
+});
+
+lightBtn.addEventListener('click', () => {
+  body.classList.add('light');
+  body.classList.remove('default', 'dark');
+  localStorage.setItem('theme', 'light');
+});
