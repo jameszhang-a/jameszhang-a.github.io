6a8f839b0355e2c999eec70438fa06b71b98cd4e
James Zhang <jameszhanga@gmail.com>
2021-08-27 16:46:52 -0700

    Add PDF support

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Document, Page } from 'react-pdf/dist/umd/entry.webpack';
 import './Resume.css';
+import PDF from './Zhang_resume_2022.pdf';
 
 export const Resume = () => {
   return (
     <div class='resume'>
       <h2>My resume</h2>
+      <br />
+
+      <Document file={PDF}>
+        <Page pageNumber={1} />
+      </Document>
     </div>
   );
 };
