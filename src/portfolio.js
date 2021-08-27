3d53c90872236afa3da262b78b4817c5a0d93a8f
James Zhang <jameszhanga@gmail.com>
2021-08-27 16:54:51 -0700

    Remove projects temp

--- src/portfolio.js
+++ src/portfolio.js
@@ -19,30 +19,30 @@ const about = {
 
 const projects = [
   // Add or edit projects
-  {
-    name        : 'Project 1',
-    description :
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack       : [ 'SASS', 'TypeScript', 'React' ],
-    sourceCode  : 'https://github.com',
-    livePreview : 'https://github.com'
-  },
-  {
-    name        : 'Project 2',
-    description :
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack       : [ 'SASS', 'TypeScript', 'React' ],
-    sourceCode  : 'https://github.com',
-    livePreview : 'https://github.com'
-  },
-  {
-    name        : 'Project 3',
-    description :
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack       : [ 'SASS', 'TypeScript', 'React' ],
-    sourceCode  : 'https://github.com',
-    livePreview : 'https://github.com'
-  }
+  // {
+  //   name        : 'Project 1',
+  //   description :
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack       : [ 'SASS', 'TypeScript', 'React' ],
+  //   sourceCode  : 'https://github.com',
+  //   livePreview : 'https://github.com'
+  // },
+  // {
+  //   name        : 'Project 2',
+  //   description :
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack       : [ 'SASS', 'TypeScript', 'React' ],
+  //   sourceCode  : 'https://github.com',
+  //   livePreview : 'https://github.com'
+  // },
+  // {
+  //   name        : 'Project 3',
+  //   description :
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack       : [ 'SASS', 'TypeScript', 'React' ],
+  //   sourceCode  : 'https://github.com',
+  //   livePreview : 'https://github.com'
+  // }
 ];
 
 const skills = [
